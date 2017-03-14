15134122cf8f116ed7cb9be1ea2e790062f07ade
Renyuan Sheng <Renyuan Sheng>
2017-03-13 22:42:10 -0400

    Add a route

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
 const request = require('request');
-const fetch = require('node-fetch');
 const APIKEY = 'a797f401a34cb50b294ca826dd7e6b01';
+// const router = require('koa-router')();
+const router = require('koa-route');
 
 function getData(URL) {
-	var data;
+	let data;
 
 	return new Promise((resolve, reject)=> {
 		request(URL, (err, res, body) => {
@@ -26,11 +27,11 @@ app.use(async function (ctx, next) {
   ctx.set('X-Response-Time', `${ms}ms`);
 });
 
+//
 app.use(async function(ctx, next){
-	let ip = ctx.ip;
+	let ip = ctx.ip; 
 	ip = '174.114.191.41';
-	await next();
-	
+
 	let LOCATIONURL = 'http://ip-api.com/json/'+ip+'?fields=520191';
 	let location = await getData(LOCATIONURL);
 	location = JSON.parse(location);
@@ -43,10 +44,20 @@ app.use(async function(ctx, next){
 	let weather = await getData(WEATHERURL);
 	weather = JSON.parse(weather);
 	console.log(weather);
-	let temp = (weather.main.temp - 273.15).toFixed(0)
+	let temp = (weather.main.temp - 273.15).toFixed(0);
 
-	ctx.body = "Hello, your location is " + lat + " " + lon + ", your current temperature is " + temp + " ℃";
+	ctx.request.lat = lat;
+	ctx.request.lon = lon;
+	ctx.request.temp = temp;
+	await next();
 });
+
+let temprature = router.get("/weather", async (ctx) => {
+	console.log(ctx.request);
+	ctx.body = `your location is ${ctx.request.lat},${ctx.request.lon}, and you current temprature is ${ctx.request.temp}`;
+});
+
+app.use(temprature);
 
 app.listen(3302, ()=>{
 	console.log("koa server starts at port 3302");
